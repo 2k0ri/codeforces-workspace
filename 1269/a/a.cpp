9d3456459e2cb03c2464eea5d7073814a4f97947
2k0ri <esc13245@gmail.com>
2019-12-23 02:20:45 +0900

    integrate with cf cli, 1269

---
+++ 1269/a/a.cpp
@@ -0,0 +1,25 @@
+// clang-format off
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,n) for(ll i=0, i##_len=(n); i<i##_len; ++i)
+#define rep1(i,n) for(ll i=1, i##_len=(n); i<=i##_len; ++i)
+#define rrep(i,n) for(ll i=n-1; i>=0; --i)
+#define rrep1(i,n) for(ll i=n; i>0; --i)
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define mp make_pair
+#define coutl cout<<fixed<<setprecision(10)
+#define ANS(ans) cout<<ans<<endl;
+template<class T>bool chmax(T &a, const T &b){if (a<b){a=b;return 1;}return 0;}
+template<class T>bool chmin(T &a, const T &b){if (b<a){a=b;return 1;}return 0;}
+const int INF = 1e9;
+const ll LINF = 1e18;
+// clang-format on
+
+int main() {
+  ll N;
+  scanf("%lld", &N);
+  ANS(N * 9 << " " << N * 8);
+  return 0;
+}
